9a2d21679db0b3185d792c2cc6b7eda57a1152e0
jpablopachar <jpablopachar1993@gmail.com>
2023-10-06 03:34:42 -0500

    Se agregan estilos Home Page

--- mobile/src/app/app.routes.ts
+++ mobile/src/app/app.routes.ts
@@ -4,7 +4,7 @@ export const routes: Routes = [
   {
     path: 'tabs',
     loadChildren: () =>
-          import('./pages/tabs/tabs.routes').then((r): Routes => r.tabsRoutes)
+      import('./pages/tabs/tabs.routes').then((r): Routes => r.tabsRoutes),
   },
   {
     path: '',
